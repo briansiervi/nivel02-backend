0685108f8f34df29a748aaffdc8c74411fcd3e5a
brian <brianmazini@gmail.com>
2020-09-07 21:43:37 -0300

    conexão com BD criada

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+import routes from './routes';
+
+import './database';
+
+const app = express();
+app.use(express.json());
+
+app.get('/', (request, response) =>
+  response.json({
+    message: 'hello GoStack',
+  }),
+);
+
+app.use(routes);
+
+app.listen(3333, () => {
+  console.log('🚀️ Server started on port 3333');
+});
