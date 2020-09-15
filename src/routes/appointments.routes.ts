1bc6ede09bbfd389fa003f1d79ed5e719fa8709e
brian <brianmazini@gmail.com>
2020-09-15 00:15:27 -0300

    Associação criada

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,5 +1,6 @@
+/* eslint-disable camelcase */
 import { Router } from 'express';
-import { getCustomRepository} from 'typeorm';
+import { getCustomRepository } from 'typeorm';
 import { parseISO } from 'date-fns';
 
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
@@ -10,13 +11,13 @@ const appointmentsRouter = Router();
 appointmentsRouter.get('/', async (request, response) => {
   const appointmentsRepository = getCustomRepository(AppointmentsRepository);
   const appointments = await appointmentsRepository.find();
-  
+
   return response.json(appointments);
 });
 
 appointmentsRouter.post('/', async (request, response) => {
   try {
-    const { provider, date } = request.body;
+    const { provider_id, date } = request.body;
 
     const parsedDate = parseISO(date);
 
@@ -24,7 +25,7 @@ appointmentsRouter.post('/', async (request, response) => {
 
     const appointment = await createAppointment.execute({
       date: parsedDate,
-      provider,
+      provider_id,
     });
 
     return response.json(appointment);
